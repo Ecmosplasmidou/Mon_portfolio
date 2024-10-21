4c831399d53ffd8846c928f64dc7981ae9a1ae1d
Ecmosplasmidou <cassamb97@gmail.>
2024-10-21 18:46:17 +0200

    mmmmm

--- includes/add_content.sql
+++ includes/add_content.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (
+CREATE TABLE IF EXISTS projects (
   id SERIAL PRIMARY KEY,
   title varchar(255) NOT NULL,
   description text NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE projects (
   cms varchar(255) DEFAULT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE IF EXISTS users (
   id SERIAL PRIMARY KEY,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
